a810f880b8cf4b2734caaacf6dc0ea6d70fe7375
BuildTools <CynthiaWang7979@hotmail.com>
2020-11-14 20:14:39 +0800

    Syntax fix

--- logic/nutrition.py
+++ logic/nutrition.py
@@ -1,33 +1,38 @@
 from util import *
+import util.json_parse as json_parse
+
+
 class Person:
     # States vars TODO:
     state={
-        "Energy":100
-        "Sodium":100
-        "Mineral":100
-        "Vitamin":100
+        "Energy": 100,
+        "Sodium": 100,
+        "Mineral": 100,
+        "Vitamin": 100
     }
+
     # Check events(?)
     def check_state(self):
         return 0
+
     # Eat and update states
-    def eat(foods=[]):
+    def eat(self, foods=()):
         for food in foods:
-            pass #TODO:update state
+            pass  # TODO:update state
         return self.check_state()
 
-    def time_pass():
-        #TODO:update state
+    def time_pass(self):
+        # TODO:update state
         return self.check_state()
     
 
 class FoodList:
-    foods={}
-    def __init__(self,path):
-        foods=json_parse.get_data(path)
+    foods = {}
+
+    def __init__(self, path):
+        foods = json_parse.get_data(path)
         for food in foods:
-            self.foods[food[name]]=food
-        
+            self.foods[food['name']]=food
 
     class Food:
         def __init__(self,):
