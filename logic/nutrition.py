c3e06aa6bed87bd72b69b0e4462975739825e1ce
EnderKerman <anatominuniverse@gmail.com>
2020-11-14 21:14:37 +0800

    appearence and branching

--- logic/nutrition.py
+++ logic/nutrition.py
@@ -2,13 +2,62 @@ from util import *
 class Person:
     # States vars TODO:
     state={
-        "Energy":100
-        "Sodium":100
-        "Mineral":100
-        "Vitamin":100
+        "Energy":100,
+        "Sodium":100,
+        "Mineral":100,
+        "Vitamin":100,
     }
-    # Check events(?)
+    apr={
+        "Age":18,#18-80
+        "Health":3,#1,2,3
+        "Size":1#0,1,2
+    }
+
+    # Check events(?) and update appearence
     def check_state(self):
+        # Update size
+        if self.state["Energy"]<50:
+            self.apr["Size"]=0
+        elif self.state["Energy"]>200:
+            self.apr["Size"]=2
+        else:
+            self.apr["Size"]=1
+
+        # Update age
+        self.apr["Age"]+=1/6
+
+        # Update health
+        self.apr["Health"]=3
+        
+        if self.state["Energy"]<25 or self.state["Energy"]>200 or\
+            self.state["Sodium"]>175 or self.state["Sodium"]<25 or\
+            self.state["Mineral"]<25 or self.state["Vitamin"]<25:
+                self.apr["Health"]=1
+
+        if self.state["Energy"]<50 or self.state["Energy"]>150 or\
+            self.state["Sodium"]>150 or self.state["Sodium"]<50 or\
+                self.state["Mineral"]<50 or self.state["Vitamin"]<50:
+                self.apr["Health"]=2
+
+
+
+
+            
+        if self.state["Sodium"]>200:
+            return 1 #Die of heart attack
+        
+        if self.state["Mineral"]<0 or self.state["Vitamin"]<0:
+            return 2 #Die of lack of essencials, maybe illusion
+        
+        if self.state["Energy"]>300:
+            return 3 #Die of obesity
+
+        if self.state["Energy"]<0:
+            return 4 #Starve to death
+
+        if self.state["Sodium"]<0:
+            return 5 #Die of nervous system breakdown, maybe illusion
+
         return 0
     # Eat and update states
     def eat(foods=[]):
