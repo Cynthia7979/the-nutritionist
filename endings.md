b4c6139eb496c6d1df8b6446513c7f4f59a7ebde
BuildTools <CynthiaWang7979@hotmail.com>
2020-11-15 14:13:53 +0800

    Endings and halfway gameplay development

---
+++ endings.md
@@ -0,0 +1,14 @@
+# Endings
+
+List of endings (spoiler alert)
+
+1. Die of heart attack
+* Click with your heartbeat: Try to catch up with
+the bumping heart
+2. Die of lack of vitamins/minerals
+3. Die of obesity
+4. Starved
+5. Die of nervous system breakdown (illusion)
+* (Insert crazy illustration of abstract things)
+6. Die of living too long
+* Game restarts and you lose your game save.
--- scenes/play.py
+++ scenes/play.py
@@ -5,6 +5,17 @@ from util import json_parse
 from logic import nutrition
 
 
+RECIPE_ROW_HEIGHT = 70
+TOP_BAR_HEIGHT = 40
+
+class MouseSprite(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.rect = pygame.Rect((x, y, 1, 1))
+
+    def group_collide(self, group):
+        return pygame.sprite.spritecollide(self, group, False)
+
 @logged_class
 class Texts(pygame.sprite.Sprite):
 
@@ -17,6 +28,7 @@ class Texts(pygame.sprite.Sprite):
         line_height = 25
 
         self.image = pygame.Surface((LEFT_DIALOG_WIDTH, LEFT_DIALOG_HEIGHT/2))
+        self.image.fill(get_gray(60))
         self.rect = self.image.get_rect(topleft=(0, LEFT_DIALOG_HEIGHT/2))
         self.logger.debug(f'{self.image}')
 
@@ -41,10 +53,9 @@ class Phone(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        self.image = pygame.Surface((PHONE_WIDTH+2, PHONE_HEIGHT+2))
+        self.image = pygame.Surface((PHONE_WIDTH, PHONE_HEIGHT))
         self.rect = self.image.get_rect(topleft=(PHONE_LEFT, PHONE_TOP))
-        pygame.draw.rect(self.image, BLACK, (0, 0, PHONE_WIDTH+2, PHONE_HEIGHT+2))  # Shadow
-        pygame.draw.rect(self.image, WHITE, (0, 0, PHONE_WIDTH, PHONE_HEIGHT))  # Screen
+        self.image.fill(get_gray(200))
         pygame.draw.rect(self.image, BLACK, (0, PHONE_HEIGHT-40, PHONE_WIDTH, 40))  # Bottom menu
 
 
@@ -52,14 +63,14 @@ class PhoneTopBar(pygame.sprite.Sprite):
     def __init__(self, text):
         super().__init__()
 
-        self.image = pygame.Surface((PHONE_WIDTH, 41))
+        self.image = pygame.Surface((PHONE_WIDTH, TOP_BAR_HEIGHT+1))
         self.image.fill(WHITE)
         self.rect = self.image.get_rect(topleft=(PHONE_LEFT, PHONE_TOP))
 
         text_surf, text_rect = render_text(text, size=24)
-        text_rect.center = (PHONE_WIDTH/2, 20)
+        text_rect.center = (PHONE_WIDTH/2, TOP_BAR_HEIGHT/2)
         self.image.blit(text_surf, text_rect)
-        pygame.draw.line(self.image, BLACK, (0, 30), (PHONE_WIDTH, 30))
+        pygame.draw.line(self.image, BLACK, (0, TOP_BAR_HEIGHT), (PHONE_WIDTH, 40))
 
     def update_text(self, text):
         self.image.fill(WHITE)
@@ -76,12 +87,41 @@ class BottomMenuButton(pygame.sprite.Sprite):
 
 
 class AppIcon(pygame.sprite.Sprite):
-    def __init__(self, top, left):
+    def __init__(self, left, top):
         super().__init__()
 
-        self.image = pygame.Surface((101, 101), flags=SRCALPHA)
-        self.rect = self.image.get_rect(topleft=(top, left))
+        self.image = pygame.Surface((101, 101))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect(topleft=(left, top))
         pygame.draw.rect(self.image, BLACK, (0, 0, 100, 100), width=1, border_radius=2)
+
+
+class RecipeRow(pygame.sprite.Sprite):
+    def __init__(self, top, food_data):
+        super().__init__()
+
+        self.image = pygame.Surface((PHONE_WIDTH, RECIPE_ROW_HEIGHT))
+        self.rect = self.image.get_rect(topleft=(PHONE_LEFT, top))
+        self.image.fill(WHITE)
+        pygame.draw.line(self.image, BLACK, (0, RECIPE_ROW_HEIGHT-1), (PHONE_WIDTH, RECIPE_ROW_HEIGHT-1))  # Bottom line
+        pygame.draw.line(self.image, BLACK, (0, 1), (PHONE_WIDTH, 1))  # Top line
+        name_surf, name_rect = render_text(food_data['name'], 20)
+        name_rect.topleft = (10, 10)
+        category_surf, category_rect = render_text(food_data['category'])
+        category_rect.topleft = (10, 40)
+        self.image.blits(((name_surf, name_rect), (category_surf, category_rect)))
+
+
+class RecipePage(pygame.sprite.Sprite):
+    def __init__(self, food):
+        super().__init__()
+
+        self.image = pygame.Surface((PHONE_WIDTH, PHONE_HEIGHT))
+        self.rect = self.image.get_rect(topleft=(PHONE_LEFT, PHONE_TOP))
+
+        title_surf, title_rect = render_text(food['name'], 30)
+        title_rect.topleft = PHONE_LEFT+20,
+        category_surf, category_rect = render_text(food['category'], 20)
 
 
 def start(display_surf, load_from):
@@ -94,6 +134,7 @@ def start(display_surf, load_from):
         player = nutrition.Person()
     else:
         player = nutrition.Person(**json_parse.get_data(load_from))
+    food_data = json_parse.get_data('food_data.json')
     messages = ['This is a message', 'This is another message', 'This is a very long message as you start to feel tired',
                 'message', 'message', 'message']
 
@@ -119,14 +160,25 @@ def start(display_surf, load_from):
     health_app.add(main_screen, main_screen_clickables)
     setting_app.add(main_screen, main_screen_clickables)
 
+    recipe_screen = pygame.sprite.Group()
+    recipe_screen_clickables = pygame.sprite.Group()
+    PhoneTopBar('Choose My Recipe').add(recipe_screen)
+    current_top = PHONE_TOP+TOP_BAR_HEIGHT+5
+    for food in food_data:
+        food_row = RecipeRow(current_top, food)
+        food_row.add(recipe_screen, recipe_screen_clickables)
+        current_top += RECIPE_ROW_HEIGHT+5
+        if current_top + RECIPE_ROW_HEIGHT >= PHONE_TOP + PHONE_HEIGHT: break
+
     active_screen = main_screen
+    last_screen = main_screen
 
     while True:
         display_surf.fill(BG_PLACEHOLDER)
         pygame.draw.rect(display_surf, get_gray(60), left_dialog_rect)
-        pygame.draw.line(display_surf, get_gray(200), (0, HEIGHT/2), (LEFT_DIALOG_WIDTH, HEIGHT/2))
+        pygame.draw.line(display_surf, get_gray(200), (0, HEIGHT/2-1), (LEFT_DIALOG_WIDTH, HEIGHT/2-1))
         general_screen.draw(display_surf)
-        main_screen.draw(display_surf)
+        active_screen.draw(display_surf)
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -134,11 +186,20 @@ def start(display_surf, load_from):
                 return QUIT, 0
             elif event.type == MOUSEBUTTONUP:  # Click event
                 mouse_pos = event.pos
-                # if some_rect.collidepoint(mouse_pos):  # Clicked on something
-                #     scene_logger.info('Clicked some_rect')
-                #     # Do something
-                # if another_rect.collidepoint(mouse_pos):
-                #     scene_logger.info('Clicked another_rect')
-                #     # Do something
+                mouse_sprite = MouseSprite(*mouse_pos)
+                if active_screen == main_screen:
+                    for collision in mouse_sprite.group_collide(main_screen_clickables):
+                        if collision == recipe_app:
+                            active_screen, last_screen = recipe_screen, active_screen
+                elif active_screen == recipe_screen:
+                    for collision in mouse_sprite.group_collide(recipe_screen_clickables):
+                        # Construct a recipe details screen
+                for collision in mouse_sprite.group_collide(general_screen_clickables):
+                    if collision == back_icon:
+                        active_screen, last_screen = last_screen, active_screen
+                    elif collision == menu_icon:
+                        active_screen, last_screen = main_screen, active_screen
         pygame.display.flip()  # Update screen
         CLOCK.tick(FPS)  # Balance loop time
+
+
